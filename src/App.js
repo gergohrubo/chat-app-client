41c69a127aee68b854aa7d8124609e5212dd92d4
gergohrubo <gergohrubo@gmail.com>
2019-12-13 17:20:07 +0100

    Client side instructional app for streaming

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import superagent from 'superagent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: ''
+  }
+  stream = new EventSource('http://localhost:4000/stream')
+  componentDidMount() {
+    this.stream.onmessage = event => {
+      console.log('event test', event.data)
+      const parsed = JSON.parse(event.data)
+      this.props.dispatch(parsed)
+    }
+  }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+  onClick = () => {
+    this.setState({ text: '' })
+  }
+  onSubmit = e => {
+    e.preventDefault()
+    const url = 'http://localhost:4000/message'
+    superagent.post(url)
+      .send(this.state)
+      .then(response => console.log(response))
+    this.onClick()
+  }
+  render() {
+    console.log('messages test:', this.props.messages)
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" name="text" onChange={this.onChange} value={this.state.text} />
+          <button>Submit</button>
+        </form>
+        <button onClick={this.onClick}>Reset</button>
+        {this.props.messages.map(message => <p key={message.id}>{message.text}</p>)}
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    messages: state
+  }
+}
+
+export default connect(mapStateToProps)(App);
